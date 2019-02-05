06c836eb9de78095055a5d34a065be671a5cd054
Christos Pavlides <cp@finapivity.com>
2019-02-05 16:26:36 +0200

    Changed Bban and CountryCode to use static dictionaries instead of creating new instance every time.

---
+++ sinkien.IBAN4Net.PerfTests/BbanBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using BenchmarkDotNet.Attributes;
+using SinKien.IBAN4Net;
+
+namespace sinkien.IBAN4Net.PerfTests
+{
+    [MemoryDiagnoser]
+    public class BbanBenchmarks
+    {        
+
+        [Benchmark(Baseline = true)]
+        public BBanStructure Base()
+        {
+            return BbanPrev.GetStructureForCountry("CY");
+        }
+
+        [Benchmark]
+        public BBanStructure StaticBBans()
+        {
+            return Bban.GetStructureForCountry("CY");
+        }
+    }
+}
