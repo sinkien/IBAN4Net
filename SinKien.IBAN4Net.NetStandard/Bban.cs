3f0ef5f0ba7773ac5fa1b23bd94344669174cd51
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@atcom.gr>
2021-02-22 22:28:27 +0200

    feat (IBAN-00019-add-support-for-exotic-countries): add support for "Honduras" IBANs

--- SinKien.IBAN4Net.NetStandard/Bban.cs
+++ SinKien.IBAN4Net.NetStandard/Bban.cs
@@ -463,6 +463,12 @@ namespace SinKien.IBAN4Net
             ));
 
             //Honduras
+            _bbanStructures.Add("HN", new BBanStructure(
+                BBanEntry.BankCode(4, "c"),
+                BBanEntry.AccountNumber(20, "n")
+            ));
+            
+            //Morocco
             _bbanStructures.Add("MA", new BBanStructure(
                 BBanEntry.BankCode(3, "n"),
                 BBanEntry.BranchCode(5, "n"),
